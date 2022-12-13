dcb4dff8cae0a6f97c3342812aaaaecb373ff486
clotildeh <c.hebert@novactive.com>
2022-12-13 11:09:56 +0100

    Add TextBlock type and add string attribute for pagebuilder

---
+++ lib/Encoder/Field/TextBlockFieldEncoder.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAutomatedTranslation\Encoder\Field;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Field;
+use Ibexa\Core\FieldType\TextBlock\Value as TextBlockValue;
+use Ibexa\Core\FieldType\Value;
+use EzSystems\EzPlatformAutomatedTranslation\Exception\EmptyTranslatedFieldException;
+
+final class TextBlockFieldEncoder implements FieldEncoderInterface
+{
+    public function canEncode(Field $field): bool
+    {
+        return $field->value instanceof TextBlockValue;
+    }
+
+    public function canDecode(string $type): bool
+    {
+        return TextBlockValue::class === $type;
+    }
+
+    public function encode(Field $field): string
+    {
+        return (string) $field->value;
+    }
+
+    public function decode(string $value, $previousFieldValue): Value
+    {
+        $value = trim($value);
+
+        if (strlen($value) === 0) {
+            throw new EmptyTranslatedFieldException();
+        }
+
+        return new TextBlockValue($value);
+    }
+}
