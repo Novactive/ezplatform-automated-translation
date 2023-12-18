2e78f5dda43d52faa3f6b3f8d96ab9c2c7c77354
Mohammed <mohamed-larbi.jebari-ext@almaviacx.com>
2023-12-18 20:45:33 +0100

    #113888 - Traduction - les traductions des langues Russes et Chinois simplifié tombent en erreur

--- lib/Client/Deepl.php
+++ lib/Client/Deepl.php
@@ -9,19 +9,21 @@ declare(strict_types=1);
 namespace EzSystems\EzPlatformAutomatedTranslation\Client;
 
 use EzSystems\EzPlatformAutomatedTranslation\Exception\ClientNotConfiguredException;
-use EzSystems\EzPlatformAutomatedTranslation\Exception\InvalidLanguageCodeException;
 use GuzzleHttp\Client;
 
 class Deepl implements ClientInterface
 {
     /** @var string */
-    private $authKey;
+    private string $authKey;
 
     /** @var string */
-    private $baseUri;
+    private string $baseUri;
 
     /** @var array */
-    private $nonSplittingTags;
+    private array $nonSplittingTags;
+
+    /** @var array */
+    private array $supportedLanguagesMapping = [];
 
     public function getServiceAlias(): string
     {
@@ -43,6 +45,10 @@ class Deepl implements ClientInterface
         if (isset($configuration['nonSplittingTags'])) {
             $this->nonSplittingTags = array_filter(explode(',', $configuration['nonSplittingTags']));
         }
+        if (isset($configuration['supported_languages_mapping'])) {
+            $this->supportedLanguagesMapping = array_unique((array) $configuration['supported_languages_mapping']);
+        }
+
     }
 
     public function translate(string $payload, ?string $from, string $to): string
@@ -87,6 +93,9 @@ class Deepl implements ClientInterface
         if (\in_array($languageCode, self::LANGUAGE_CODES)) {
             return $languageCode;
         }
+        if (isset($this->supportedLanguagesMapping[$languageCode])) {
+            return $this->supportedLanguagesMapping[$languageCode];
+        }
 
         $code = strtoupper(substr($languageCode, 0, 2));
         if (\in_array($code, self::LANGUAGE_CODES)) {
@@ -97,7 +106,10 @@ class Deepl implements ClientInterface
     }
 
     /**
-     * List of available code https://www.deepl.com/api.html.
+     * List of available code https://www.deepl.com/docs-api/translate-text
      */
-    private const LANGUAGE_CODES = ['EN', 'DE', 'FR', 'ES', 'IT', 'NL', 'PL', 'JA'];
+    private const LANGUAGE_CODES = ['BG', 'CS','DA', 'DE', 'EL', 'EN','EN-GB','EN-US', 'ES', 'ET',
+        'FI','FR', 'HU', 'ID', 'IT', 'JA', 'KO', 'LT', 'LV', 'NB', 'NL', 'PL', 'PT', 'PT-BR', 'PT-PT', 'RO',
+         'RU', 'SK', 'SL', 'SV', 'TR', 'UK', 'ZH'
+    ];
 }
